d89b85415a1dfc5e598df0c6926665bd5bb2a813
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-10-25 21:15:10 +0800

    added slideshow fn()

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const imgs = [
 ];
 
 let imgIndex = 0;
-
+let intervalId = null;
 function parseIndex(index) {
   if (index > imgs.length - 1) {
     return 0;
@@ -25,15 +25,27 @@ function parseIndex(index) {
   }
   return index;
 }
+function highlightDot(index) {
+  dotsContainer
+    .querySelectorAll(".dots")
+    .forEach((dot) => dot.classList.remove("dots-hl"));
+
+  dotsContainer
+    .querySelector(`.dots[data-index='${index}']`)
+    .classList.add("dots-hl");
+}
+
 nextBtn.addEventListener("click", () => {
   imgIndex++;
   imgIndex = parseIndex(imgIndex);
+  highlightDot(imgIndex);
   mainFrame.src = imgs[imgIndex].src;
 });
 
 prevBtn.addEventListener("click", () => {
   imgIndex--;
   imgIndex = parseIndex(imgIndex);
+  highlightDot(imgIndex);
   mainFrame.src = imgs[imgIndex].src;
 });
 for (let i = 0; i <= imgs.length - 1; i++) {
@@ -46,6 +58,25 @@ for (let i = 0; i <= imgs.length - 1; i++) {
 dotsContainer.addEventListener("click", (e) => {
   if (!e.target.className.includes("dots")) return;
   imgIndex = +e.target.dataset.index;
+  highlightDot(imgIndex);
   console.log(imgIndex);
   mainFrame.src = imgs[imgIndex].src;
 });
+
+slide.addEventListener("click", () => {
+  if (slide.className.includes("sliding")) {
+    slide.classList.remove("sliding");
+    slide.textContent = "Start Slideshow";
+    clearInterval(intervalId);
+  } else {
+    slide.classList.add("sliding");
+    slide.textContent = "Stop Slideshow";
+    intervalId = setInterval(() => {
+      imgIndex++;
+      imgIndex = parseIndex(imgIndex);
+      highlightDot(imgIndex);
+      mainFrame.src = imgs[imgIndex].src;
+    }, 2000);
+  }
+});
+highlightDot(0);
